a1c6bf35d71f28af3c4cbd6799a28a8307fd904d
Artem <impulsar007@gmail.com>
2024-11-06 18:57:33 +0200

    v0.0.1

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -2,7 +2,36 @@ package cli
 
 import (
 	"flag"
+	"fmt"
+	"local-ci/cmd/config"
+	"local-ci/cmd/docker"
+	"os"
 )
 
-var FilePath = flag.String("f", "local.yaml", "path to config file")
-var Foo = flag.String("foo", "", "foo config file")
+func Start(args []string) {
+	fs := flag.NewFlagSet("start", flag.ExitOnError)
+	filePath := fs.String(
+		"f", ".local-ci/local.yaml",
+		"path to config file (defaults to .local-ci/local.yaml)",
+	)
+	fs.String("help", "", "type local-ci start to start the pipeline")
+	if err := fs.Parse(args); err != nil {
+		fmt.Printf("error: %s", err)
+		return
+	}
+	fmt.Printf("Getting config from file: %v", *filePath)
+	pwd, _ := os.Getwd()
+	fmt.Printf("Working dir: %s\n", pwd)
+	yamlConf := config.Config{}
+	err := yamlConf.GetConfig(pwd + "/" + *filePath)
+	if err != nil {
+		panic(err)
+	}
+	errVal := config.ValidateConfig(yamlConf)
+	if errVal != nil {
+		panic(errVal)
+	}
+	for item := range yamlConf.Blocks {
+		docker.ExecuteConfigPipeline(yamlConf.Blocks[item])
+	}
+}
--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"local-ci/cmd/cli"
+	"os"
 )
 
 func main() {
-	flag.Parse()
-	fmt.Println(*cli.Foo)
-	//pwd, _ := os.Getwd()
-	//fmt.Printf("Working dir: %s\n", pwd)
-	//yamlConf := config.Config{}
-	//err := yamlConf.GetConfig(*cli.FilePath)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//errVal := config.ValidateConfig(yamlConf)
-	//if errVal != nil {
-	//	panic(errVal)
-	//}
-	//for item := range yamlConf.Blocks {
-	//	fmt.Println(item)
-	//	fmt.Println(yamlConf.Blocks[item].Variables["FOO"])
-	//	fmt.Println(yamlConf.Blocks[item])
-	//	docker.ExecuteConfigPipeline(yamlConf.Blocks[item])
-	//}
-
+	fmt.Println(os.Args[2:])
+	if os.Args[1] == "start" {
+		cli.Start(os.Args[2:])
+	}
 }
